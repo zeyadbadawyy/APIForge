a33cfeae90845e7c341bd391e6678db1a36cd94b
Zeyad Badawy <zeyadbadawyy@gmail.com>
2026-06-18 00:56:42 +0300

    Add health check, statistics, filtering and environment configuration

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"apiforge/internal/routes"
+	"apiforge/internal/storage"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	storage.LoadTasks()
 	router := routes.RegisterRoutes()
 
 	fmt.Println(
 		"APIForge running on :8080",
 	)
+	godotenv.Load()
+
+	port :=
+		os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 
 	http.ListenAndServe(
-		":8080",
+		":"+port,
 		router,
 	)
 }
